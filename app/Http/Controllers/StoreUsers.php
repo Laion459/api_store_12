22ec22c3d9f96c806b89fb256ebae9d14a457ca6
laion459 <laion459@gmail.com>
2024-03-04 16:29:47 -0300

    initial commit

---
+++ app/Http/Controllers/StoreUsers.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\store;
+use Illuminate\Http\Request;
+
+class StoreUsers extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(store $store)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, store $store)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(store $store)
+    {
+        //
+    }
+}
